c6cca01d0f9f3bcee9a2faff23e8f8853cbec075
samuel cordova <cordovas1337@gmail.com>
2022-06-25 04:57:40 -0500

    added some javascript to randomly generate computer choice

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
-const computerChoice = document.getElementById('computer-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
+let computerChoice
+let result
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML= userChoice
+    generateComputerChoice()
+    getResult()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1 //same as putting the number 3 in this case
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    }
+    if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+
+}
